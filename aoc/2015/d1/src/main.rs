183763a9b42cc27f154ac62c75dc0c3d44dcbc0d
kramer9 <kramer92002@gmail.com>
2023-10-17 14:18:03 -0400

    pulling from a file

--- aoc/2015/d1/src/main.rs
+++ aoc/2015/d1/src/main.rs
@@ -1,3 +1,14 @@
+// https://adventofcode.com/2015/day/1
+
+use std::fs;
+
 fn main() {
-    println!("Hello, world!3");
+    let file_path = "/home/argus/Drives/2TBa/Projects/rust-Linux/aoc/2015/d1/src/input.txt";
+    // --snip--
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
