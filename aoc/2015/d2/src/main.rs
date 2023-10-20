e657ab06d834b2907482b7d3a0022e7b8b84a793
kramer9 <kramer92002@gmail.com>
2023-10-20 08:31:20 -0400

    read in input

--- aoc/2015/d2/src/main.rs
+++ aoc/2015/d2/src/main.rs
@@ -5,6 +5,16 @@
 
 */
 
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //hardcoding the variable rather then accepting it via command line
+    let file_path: &str = "/home/argus/Drives/2TBa/Projects/rust-Linux/aoc/2015/d1/src/input.txt";
+    // --snip--
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    
+    println!("With text:\n{contents}");
 }
